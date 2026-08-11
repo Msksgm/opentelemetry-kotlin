d6e227692c151d44a0db26b550c100d7ec38216e
Jamie Lynch <fractalwrench@gmail.com>
2026-08-11 11:05:28 +0100

    fix conversion of AnyValue (#757)

--- compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/init/CompatOpenTelemetryConfig.kt
+++ compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/init/CompatOpenTelemetryConfig.kt
@@ -5,7 +5,7 @@ import io.opentelemetry.kotlin.ExperimentalApi
 import io.opentelemetry.kotlin.aliases.OtelJavaResource
 import io.opentelemetry.kotlin.attributes.AttributesMutator
 import io.opentelemetry.kotlin.attributes.CompatAttributesModel
-import io.opentelemetry.kotlin.attributes.setAttributes
+import io.opentelemetry.kotlin.attributes.setTypedAttributes
 import io.opentelemetry.kotlin.error.GuardedSdkErrorHandler
 import io.opentelemetry.kotlin.error.NoopSdkErrorHandler
 import io.opentelemetry.kotlin.error.SdkErrorHandler
@@ -55,7 +55,7 @@ internal class CompatOpenTelemetryConfig(
     }
 
     override fun resource(map: Map<String, Any>) {
-        globalResourceAttrs.apply { setAttributes(map) }
+        globalResourceAttrs.apply { setTypedAttributes(map) }
     }
 
     internal fun buildGlobalResource(): Resource =

3031ad1545234d70b8af97b554c4efa38c36ca7d
Jamie Lynch <fractalwrench@gmail.com>
2026-08-14 10:07:02 +0100

    wait for operation result code (#832)

--- compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/logging/export/LogRecordExporterAdapter.kt
+++ compat/src/jvmAndAndroidMain/kotlin/io/opentelemetry/kotlin/logging/export/LogRecordExporterAdapter.kt
@@ -1,10 +1,10 @@
 package io.opentelemetry.kotlin.logging.export
 
 import io.opentelemetry.kotlin.aliases.OtelJavaLogRecordExporter
+import io.opentelemetry.kotlin.awaitOperationResultCode
 import io.opentelemetry.kotlin.export.MutableShutdownState
 import io.opentelemetry.kotlin.export.OperationResultCode
 import io.opentelemetry.kotlin.logging.data.LogRecordData
-import io.opentelemetry.kotlin.toOperationResultCode
 
 internal class LogRecordExporterAdapter(
     private val impl: OtelJavaLogRecordExporter
@@ -14,14 +14,16 @@ internal class LogRecordExporterAdapter(
 
     override suspend fun export(telemetry: List<LogRecordData>): OperationResultCode =
         shutdownState.ifActive {
-            impl.export(telemetry.map(LogRecordData::toOtelJavaLogRecordData))
-                .toOperationResultCode()
+            awaitOperationResultCode {
+                impl.export(telemetry.map(LogRecordData::toOtelJavaLogRecordData))
+            }
         }
 
-    override suspend fun forceFlush(): OperationResultCode = impl.flush().toOperationResultCode()
+    override suspend fun forceFlush(): OperationResultCode =
+        awaitOperationResultCode { impl.flush() }
 
     override suspend fun shutdown(): OperationResultCode =
         shutdownState.shutdown {
-            impl.shutdown().toOperationResultCode()
+            awaitOperationResultCode { impl.shutdown() }
         }
 }
